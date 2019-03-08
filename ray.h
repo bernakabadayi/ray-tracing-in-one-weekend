d083df86a59f1df4d9dce389c3241476d12a5333
Berna Kabadayi <bernakabadayi2@gmail.com>
2019-03-09 00:00:13 +0300

    Surface normals

--- ray.h
+++ ray.h
@@ -14,7 +14,7 @@ class ray {
 
     public:
         ray(){}
-        ray(const vec3& a , const vec3& b){ A = b; B = b ;}
+        ray(const vec3& a , const vec3& b){ A = a; B = b ;}
         vec3 origin() const { return A; }
         vec3 direction() const { return B; }
         vec3 point_at_parameter(float t){return A + t*B ;}
